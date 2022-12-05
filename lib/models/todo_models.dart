02db8fd549fd78311960498b373b408f9eb57e33
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2022-12-05 07:55:35 +0100

    ✨ modified: Model class

--- lib/models/todo_models.dart
+++ lib/models/todo_models.dart
@@ -3,7 +3,9 @@ class TodoModel {
 
   String? todo;
 
-  //return a Map; Key value pair of to
+  //return a Map; Key-Value pair of Todo Model ;
+  //Needed cos most database function required it to be a MAP
+
   Map<String, dynamic> toMap() {
     return {"todo": todo};
   }
